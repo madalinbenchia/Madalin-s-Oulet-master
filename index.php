03c83f91b3bfa1e7a9b76a08605c73f88b911899
madalinbenchia <33199272+madalinbenchia@users.noreply.github.com>
2018-04-15 00:33:18 +0300

    Create main page

--- index.php
+++ index.php
@@ -5,10 +5,65 @@
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<meta name="viewport" content="width = device-width, initial-scale = 1, user-scalable = no">
-	<script src="https://ajax.googleis.com/ajax/libs/jquery/2.1.2/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>	
 </head>
 <body>
+	<!-- Top Nav Bar -->
+	<nav class="navbar navbar-default navbar-fixed-up">
+		<div class="container">
+			<a href="index.php" class="navbar-brand">Madalin's Outlet</a>
+			<ul class="nav navbar-nav">
+				<li class="dropdown">
+					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Men <span class="caret"></span></a>
+					<ul class="dropdown-menu" role="menu">
+						<li><a href="#">Shirts</a></li>
+						<li><a href="#">Pants</a></li>
+						<li><a href="#">Shoes</a></li>
+						<li><a href="#">Accessories</a></li>
+					</ul>
+				</li>
+			</ul>	
+		</div>
+	</nav>
 
+	<!-- Header -->
+	<div id="headerWrapper">
+		<div id="back-flower"></div>
+		<div id="logoText"></div>
+		<div id="for-flower"></div>
+	</div>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras erat ligula, euismod non blandit sed, pharetra sed quam. Phasellus laoreet egestas sem sit amet sodales. Donec ex urna, euismod vel ante sit amet, lobortis hendrerit nunc. Nullam erat nunc, scelerisque vitae euismod sed, venenatis a quam. Morbi quis mauris laoreet, mollis urna euismod, sollicitudin magna. Curabitur egestas mauris enim, eget porta sem feugiat et. Duis tristique lobortis lobortis.</p>
+	
+
+	
+
+
+	<script>
+		jQuery(window).scroll(function(){
+
+			var vscroll = jQuery(this).scrollTop();
+			jQuery('#logoText').css({
+				"transform" : "translate(0px, "+vscroll/2 +"px)"
+			});
+
+			var vscroll = jQuery(this).scrollTop();
+			jQuery('#back-flower').css({
+				"transform" : "translate("+vscroll/5+"px, -"+vscroll/12 +"px)"
+			});
+
+			var vscroll = jQuery(this).scrollTop();
+			jQuery('#for-flower').css({
+				"transform" : "translate(0px, -"+vscroll/2 +"px)"
+			});
+		});
+	</script>
 </body>
 </html>
