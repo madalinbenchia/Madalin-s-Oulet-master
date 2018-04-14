1b237ea645bd0b84eadb05e50b357a3ce9f78738
madalinbenchia <33199272+madalinbenchia@users.noreply.github.com>
2018-04-15 00:32:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Madalin-s-Oulet-master
---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Madalin's Outlet</title>
+	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<meta name="viewport" content="width = device-width, initial-scale = 1, user-scalable = no">
+	<script src="https://ajax.googleis.com/ajax/libs/jquery/2.1.2/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>	
+</head>
+<body>
+
+</body>
+</html>
